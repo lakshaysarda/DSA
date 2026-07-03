1ccf4594005e2a6b6c21771f3b2cdede4283c50c
lakshaysarda <lakshaysarda@gmail.com>
2026-07-03 17:11:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Length of Linked List/length-of-linked-list.cpp
@@ -0,0 +1,27 @@
+/* Structure of linked list Node
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    int getCount(Node* head) {
+        
+        Node * temp = head ;
+        int count =0;
+        
+        while (temp){
+            count++;
+            temp=temp->next;
+        }
+        return count ;
+        
+    }
+};
