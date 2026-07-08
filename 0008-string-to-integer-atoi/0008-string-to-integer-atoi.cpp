6378f565d78b0bf1aba98002110d927eb18664d8
lakshaysarda <lakshaysarda@gmail.com>
2026-07-09 00:20:44 +0530

    Time: 0 ms (100%), Space: 9.1 MB (55.88%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,48 +1,62 @@
 class Solution {
 public:
-    int myAtoi(string s) {
-int i =0;
-int n = s.size();
-        
-        while ( i < n && s[i] == ' ' ){
-            i++;
-        }
-        //leading zeroes 
-        
 
-         int sign = 1;
-        if (i < n && (s[i] == '+' || s[i] == '-')) {
-            if (s[i] == '-')
-                sign = -1;
-            i++;
-        }
+    long long digitmaker(string &s, int i, long long ans, bool negative) {
 
-        
+        // Stop if end of string or current character is not a digit
+        if (i == s.size() || !isdigit(s[i]))
+            return ans;
 
-        
-
-     long long num = 0;
-
-        // Read digits
-        while (i < n && isdigit(s[i])) {
-
-            if (!isdigit(s[i]))
-    break;
-
-            num = num * 10 + (s[i] - '0');
-
-            // Handle overflow
-            if (sign * num > INT_MAX)
+        // Check overflow before adding the next digit
+        if (!negative) {
+            if (ans > (INT_MAX - (s[i] - '0')) / 10)
                 return INT_MAX;
-
-            if (sign * num < INT_MIN)
-                return INT_MIN;
-
-            i++;
+        } else {
+            if (ans > (-(long long)INT_MIN - (s[i] - '0')) / 10)
+                return -(long long)INT_MIN;
         }
 
-        return sign * num;
+        ans = ans * 10 + (s[i] - '0');
+
+        return digitmaker(s, i + 1, ans, negative);
     }
 
-       
+    int myAtoi(string s) {
+
+        int i = 0;
+
+        // Skip leading spaces
+        while (i < s.size() && s[i] == ' ')
+            i++;
+
+        // Empty string after spaces
+        if (i == s.size())
+            return 0;
+
+        bool negative = false;
+
+        // Check sign
+        if (s[i] == '+' || s[i] == '-') {
+            if (s[i] == '-')
+                negative = true;
+            i++;
+        }
+
+        // First character after sign must be a digit
+        if (i == s.size() || !isdigit(s[i]))
+            return 0;
+
+        long long ans = digitmaker(s, i, 0, negative);
+
+        if (negative) {
+            if (ans == -(long long)INT_MIN)
+                return INT_MIN;
+            return -ans;
+        }
+
+        if (ans == INT_MAX)
+            return INT_MAX;
+
+        return ans;
+    }
 };
