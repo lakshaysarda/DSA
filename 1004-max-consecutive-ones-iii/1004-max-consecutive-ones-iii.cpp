e573d6617eacd048a6d0f6e158ddc85fc880a5ee
lakshaysarda <lakshaysarda@gmail.com>
2026-07-15 14:43:17 +0530

    Time: 0 ms (100%), Space: 69.6 MB (61.3%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,54 +1,19 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-         if (k == 0) {
-            int cnt = 0, ans = 0;
-            for (int x : nums) {
-                if (x == 1) {
-                    cnt++;
-                    ans = max(ans, cnt);
-                } else {
-                    cnt = 0;
-                }
+        int left = 0, zeros = 0, maxi = 0;
+        
+        for (int right = 0; right < nums.size(); right++) {
+            if (nums[right] == 0) zeros++;      // expand window, track zero count
+            
+            while (zeros > k) {                  // window has too many zeros
+                if (nums[left] == 0) zeros--;    // uncount the zero we're kicking out
+                left++;                          // shrink from the left
             }
-            return ans;
+            
+            maxi = max(maxi, right - left + 1);  // window [left, right] is valid
         }
         
-        int n = nums.size();
-        int left = 0;
-        int right =0;
-        int maxi =0;
-        queue<pair<int,int>>qu;
-        
-        
-
-        while(right<n){
-        if (nums[right] == 0){
-            // yaha <k hai kyonki agar k=1 bhi hota toh initially qsize =0 toh ek element fit kr paate and then bahar
-            if (qu.size()<k) {
-               qu.push({nums[right], right});
-                
-                
-                maxi = max(maxi , right -left+1);
-                right++;
-            } else{
-                
-                left = qu.front().second +1;
-                qu.pop();
-                qu.push({0, right});
-                
-                
-                maxi = max(maxi , right -left+1);
-                right ++;
-
-            }
-        } 
-        else{
-            maxi = max(maxi , right -left+1);
-                right++;
-
-        }
-        }
         return maxi;
     }
 };
