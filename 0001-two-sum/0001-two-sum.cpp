73cb6eb3af145023469a638a6b4102f7e6d17653
lakshaysarda <lakshaysarda@gmail.com>
2026-07-28 20:05:01 +0530

    Time: 3 ms (66.44%), Space: 15.1 MB (12.04%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<long long , int> maps;
+        
+
+
+        for ( int i =0 ; i< nums.size() ; i++) {
+        
+
+        
+        int rem = target - nums[i];
+        if (maps.find(rem)  != maps.end()) {
+       
+        return {maps[rem],i}; // this is alot to learn as we can return like this in a vector i dont need a temp vector to stotre values and then return 
+        } 
+        maps[nums[i]]  =i;
+        } return {};
+    }
+};
