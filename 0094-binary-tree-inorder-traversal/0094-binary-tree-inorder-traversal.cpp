038b2b988d7a7b8f525b9451ab2a54d246e92beb
lakshaysarda <lakshaysarda@gmail.com>
2026-08-10 19:50:37 +0530

    Time: 0 ms (100%), Space: 10.9 MB (34.83%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -3,25 +3,52 @@ public:
     vector<int> inorderTraversal(TreeNode* root) {
 
         vector<int> ans;
-        stack<TreeNode*> st;
+
         TreeNode* curr = root;
 
-        while (curr != nullptr || !st.empty()) {
+        while (curr != NULL) {
 
-            // Go to the leftmost node
-            while (curr != nullptr) {
-                st.push(curr);
-                curr = curr->left;
+            // Case 1: No left child
+            if (curr->left == NULL) {
+
+                ans.push_back(curr->val);
+
+                curr = curr->right;
             }
 
-            // Visit the node
-            curr = st.top();
-            st.pop();
+            // Case 2: Left child exists
+            else {
 
-            ans.push_back(curr->val);
+                // Find predecessor
+                TreeNode* prev = curr->left;
 
-            // Move to the right subtree
-            curr = curr->right;
+                while (prev->right != NULL && prev->right != curr) {
+                    prev = prev->right;
+                }
+
+                // First time visiting curr
+                if (prev->right == NULL) {
+
+                    // Create thread
+                    prev->right = curr;
+
+                    // Go to left subtree
+                    curr = curr->left;
+                }
+
+                // Second time visiting curr
+                else {
+
+                    // Remove thread
+                    prev->right = NULL;
+
+                    // NOW visit curr
+                    ans.push_back(curr->val);
+
+                    // Go right
+                    curr = curr->right;
+                }
+            }
         }
 
         return ans;
