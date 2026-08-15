ae581d4c507307d281c265d5e58f9de5604e5d5d
lakshaysarda <lakshaysarda@gmail.com>
2026-08-16 03:15:50 +0530

    Time: 103 ms (16.88%), Space: 82.1 MB (21.9%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
 
-    int distance(int x, int y) {
-        return x*x + y*y;
-    }
+    struct comp {
+        bool operator()(vector<int>& a, vector<int>& b) {
+
+            int d1 = a[0]*a[0] + a[1]*a[1];
+            int d2 = b[0]*b[0] + b[1]*b[1];
+
+            return d1 < d2;
+        }
+    };
 
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
 
-        map<int, vector<vector<int>>> mpp;
+        priority_queue<vector<int>,
+                       vector<vector<int>>,
+                       comp> pq;
 
         for (auto point : points) {
-            int d = distance(point[0], point[1]);
-            mpp[d].push_back(point);
+
+            pq.push(point);
+
+            if (pq.size() > k)
+                pq.pop();
         }
 
         vector<vector<int>> ans;
 
-        for (auto it : mpp) {
-
-            for (auto point : it.second) {
-
-                ans.push_back(point);
-
-                if (ans.size() == k)
-                    return ans;
-            }
+        while (!pq.empty()) {
+            ans.push_back(pq.top());
+            pq.pop();
         }
 
         return ans;
