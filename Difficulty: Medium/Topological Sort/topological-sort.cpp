b578692691eb165d65774b93eb1b94188fdf6afc
lakshaysarda <lakshaysarda@gmail.com>
2026-08-26 14:24:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -1,47 +1,55 @@
 class Solution {
   public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        
+        vector<vector<int>> adj(V);
 
-      void dfs(int node, vector<vector<int>>& adj, 
-               vector<int>& vis, stack<int>& st) {
-
-          vis[node] = 1;
-
-          for (auto neighbour : adj[node]) {
-              if (!vis[neighbour]) {
-                  dfs(neighbour, adj, vis, st);
+        // Create adjacency list
+        for (auto edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+        }
+        
+        // make a queue and satrt doing bfs traverssal
+        //make a vector to maek the indegree
+        
+      ;
+        vector<int>ans;
+        
+        
+         vector<int> ind(V, 0);
+          for (int u = 0; u < V; u++) {
+              for (int v : adj[u]) {
+                  ind[v]++;
               }
           }
-
-          // Push after visiting all neighbours
-          st.push(node);
-      }
-
-      vector<int> topoSort(int V, vector<vector<int>>& edges) {
-
-          vector<vector<int>> adj(V);
-
-          // Create adjacency list
-          for (auto edge : edges) {
-              adj[edge[0]].push_back(edge[1]);
-          }
-
-          vector<int> vis(V, 0);
-          stack<int> st;
-
-          // DFS from every node
-          for (int i = 0; i < V; i++) {
-              if (!vis[i]) {
-                  dfs(i, adj, vis, st);
-              }
-          }
-
-          vector<int> ans;
-
-          while (!st.empty()) {
-              ans.push_back(st.top());
-              st.pop();
-          }
-
-          return ans;
-      }
-  };
+        
+        
+        queue<int>q;
+        // ismein humne sabse pehle 0 wala banda dundhna hai 
+        for ( int i =0 ; i<V;i++){
+            if (ind[i]==0){
+                q.push(i);
+                
+            }
+        }
+        
+        while (!q.empty()){
+            // then humein yeh undone krna hai unko nikal and then unke connectors ko indone kro 
+            int node = q.front();
+            q.pop();
+            for( int i =0 ; i < adj[node].size();i++){
+                ind[adj[node][i]]--;
+                
+                if ( ind[adj[node][i]] ==0 )q.push(adj[node][i]);
+               
+            } 
+            ans.push_back(node);
+            
+            
+        }
+        return ans ;
+        
+        
+    }
+    
+};
